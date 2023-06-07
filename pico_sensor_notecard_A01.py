f4aad7e04362c4418b965b8c59041454dae69fac
johnedstone <johnedstone@gmail.com>
2023-06-06 22:54:20 -0400

    gps finished, ready to send request to notehub

--- pico_sensor_notecard_A01.py
+++ pico_sensor_notecard_A01.py
@@ -11,7 +11,7 @@ START_TIME = 0
 DEBUG = True
 CARD_RESTORE = False
 
-led = (Pin(25), Pin.OUT)
+led_onboard = Pin(25, Pin.OUT)
 i2c_bme680 = I2C(0, sda=Pin(0), scl=Pin(1))
 i2c_notecarrier = I2C(1, sda=Pin(6), scl=Pin(7))
 
@@ -26,8 +26,9 @@ if DEBUG:
 bme680_sensor = adafruit_bme680.BME680_I2C(i2c_bme680, address=i2c_bme680_addr)
 card = notecard.OpenI2C(i2c_notecarrier, i2c_notecarrier_addr, 0, debug=True)
 
-def set_start_time(card):
+def set_start_time():
     global START_TIME
+    led_onboard.value(1)
     counter = 1
     while START_TIME == 0:
         req = {"req": "card.time"}
@@ -40,8 +41,41 @@ def set_start_time(card):
             START_TIME = rsp['time']
             if DEBUG:
                 print(f'START_TIME: {START_TIME}')
-        sleep(10)
-        
+        else:
+            sleep(10)
+
+    led_onboard.value(0)
+    return
+
+def start_gps():
+    req = {"req": "card.location.mode"}
+    req["mode"] = "periodic"
+    req["seconds"] = 3600
+    rsp = card.Transaction(req)
+
+    if DEBUG:
+        print(rsp)
+
+    gps_location_off = True
+    while gps_location_off:
+        req = {"req": "card.location"}
+        rsp = card.Transaction(req)
+
+        if 'lat' in rsp.keys():
+            gps_location_off = False
+            led_onboard.value(0)
+        else:
+            for i in range(5):
+                led_onboard.value(1)
+                sleep(2)
+                led_onboard.value(0)
+                sleep(2)
+
+        if DEBUG:
+            print(rsp)
+
+    return
+
 if CARD_RESTORE:
     req = {"req": "card.restore"}
     req["delete"] = True
@@ -54,9 +88,15 @@ req["product"] = secrets.productUID
 req["mode"] = "periodic"
 rsp = card.Transaction(req)
 
-_ = set_start_time(card)
+_ = set_start_time()
 
+_ = start_gps()
 
+sleep(5) # to let sensors settle in
+
+while True:
+    print(f'bme680_sensor: {START_TIME} {bme680_sensor.temperature:.2f}C {(bme680_sensor.temperature*9/5)+32:.2f}F, {bme680_sensor.pressure:.2f}hPa, {bme680_sensor.humidity:.2f}%RH')
+    sleep(300)
 
 
 # vim: ai et ts=4 sw=4 sts=4 nu
