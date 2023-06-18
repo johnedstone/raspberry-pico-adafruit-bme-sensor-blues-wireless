734f1f5d8ff37eeb3b8e39b08b753513fd4d4774
johnedstone <johnedstone@gmail.com>
2023-06-17 21:37:07 -0400

    cleaning up

--- compare_sensors_and_lib.py
+++ compare_sensors_and_lib.py
@@ -7,15 +7,18 @@ import bme280
 import bme280_float
 import bme280_int
 
+BME280 = False
+
 i2c_bme680 = I2C(0, sda=Pin(0), scl=Pin(1))
 i2c_bme680_addr = i2c_bme680.scan()[0]
 print(f'i2c_bme680: {i2c_bme680}')
 print(f'i2c_bme680.scan()[0]: {i2c_bme680_addr}')
 
-i2c_bme280 = I2C(1, sda=Pin(2), scl=Pin(3))
-i2c_bme280_addr = i2c_bme280.scan()[0]
-print(f'i2c_bme280: {i2c_bme280}')
-print(f'i2c_bme280.scan(): {i2c_bme280_addr}')
+if BME280:
+    i2c_bme280 = I2C(1, sda=Pin(2), scl=Pin(3))
+    i2c_bme280_addr = i2c_bme280.scan()[0]
+    print(f'i2c_bme280: {i2c_bme280}')
+    print(f'i2c_bme280.scan(): {i2c_bme280_addr}')
 
 print(f'{"#"*40}')
 
@@ -31,13 +34,14 @@ print(f'bme680_sensor_rhi: {bme680_sensor_rhh.temperature} {bme680_sensor_rhh.hu
 
 print(f'{"#"*40}')
 
-bme280_sensor_ada = bme280.BME280(i2c=i2c_bme280, address=i2c_bme280_addr)
-bme280_sensor_rhh = bme280_float.BME280(i2c=i2c_bme280, address=i2c_bme280_addr)
-bme280_sensor_rhh_mode_1 = bme280_float.BME280(mode=1, i2c=i2c_bme280, address=i2c_bme280_addr)
-bme280_sensor_rhi = bme280_int.BME280(i2c=i2c_bme280, address=i2c_bme280_addr)
-print(f'bme280_sensor_ada: {bme280_sensor_ada.values}')
-print(f'bme280_sensor_rhh: {bme280_sensor_rhh.values}')
-print(f'bme280_sensor_rhh_mode_1: {bme280_sensor_rhh_mode_1.values}')
-print(f'bme280_sensor_rhi: {bme280_sensor_rhi.values}')
+if BME280:
+    bme280_sensor_ada = bme280.BME280(i2c=i2c_bme280, address=i2c_bme280_addr)
+    bme280_sensor_rhh = bme280_float.BME280(i2c=i2c_bme280, address=i2c_bme280_addr)
+    bme280_sensor_rhh_mode_1 = bme280_float.BME280(mode=1, i2c=i2c_bme280, address=i2c_bme280_addr)
+    bme280_sensor_rhi = bme280_int.BME280(i2c=i2c_bme280, address=i2c_bme280_addr)
+    print(f'bme280_sensor_ada: {bme280_sensor_ada.values}')
+    print(f'bme280_sensor_rhh: {bme280_sensor_rhh.values}')
+    print(f'bme280_sensor_rhh_mode_1: {bme280_sensor_rhh_mode_1.values}')
+    print(f'bme280_sensor_rhi: {bme280_sensor_rhi.values}')
 
 # vim: ai et ts=4 sw=4 sts=4 nu
