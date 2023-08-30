c044ef18332cd4a42fa233d0a26c9ea1ca0cdaa9
johnedstone <johnedstone@gmail.com>
2023-08-29 20:12:39 -0400

    working on chords

--- extras/mcu_music/README.md
+++ extras/mcu_music/README.md
@@ -3,6 +3,7 @@
 
 ### References:
 * [rp2040 mp3](https://learn.adafruit.com/mp3-playback-rp2040)
+* [musical notes](https://learn.adafruit.com/circuit-playground-music/the-sound-of-music)
 * [adafruit switches](https://learn.adafruit.com/make-it-switch/other-types-of-switches)
     * [coding switches](https://learn.adafruit.com/make-it-switch/code-your-micro)
     * [debouncing](https://learn.adafruit.com/key-pad-matrix-scanning-in-circuitpython/keys-one-key-per-pin)
--- extras/mcu_music/two_button.py
+++ extras/mcu_music/two_button.py
@@ -30,61 +30,22 @@ decoder = audiomp3.MP3Decoder(open("/music/somewhere_01.mp3", "rb"))
 note_C4 = get_note(262)
 note_E4 = get_note(330)
 note_G4 = get_note(392)
-
-#button_note_C4 = DigitalInOut(board.GP1)
-#button_note_C4.direction = Direction.INPUT
-#button_note_C4.pull = Pull.UP
-
-#button_note_E4 = DigitalInOut(board.GP3)
-#button_note_E4.direction = Direction.INPUT
-#button_note_E4.pull = Pull.UP
-
-#if not True:
-#    audio.play(decoder)
-#    while audio.playing:
-#        pass
-#
-#    print("Done playing!")
-#if not True:
-#    #length = 8000 // tone_frequency
-#    #sine_wave = array.array("H", [0] * length)
-#    #for index in range(length):
-#    #    sine_wave[index] = int((1 + math.sin(math.pi * 2 * index / length))
-#    #                           * tone_volume * (2 ** 15 - 1))
-#    #sine_wave_sample = RawSample(sine_wave)
-#    sine_wave_sample = get_note(note_C4_freq)
-#
-#    #print(f'tone_frequency: {tone_frequency}')
-#    audio.play(sine_wave_sample, loop=True)
-#    time.sleep(1.5)
-#    audio.stop()
-#    time.sleep(1)
-#
-#
-#while not True:
-#    if not button_note_C4.value:
-#        print("Play C4")
-#        audio.play(note_C4, loop=True)
-#    elif not button_note_E4.value:
-#        print("Play E4")
-#        audio.play(note_E4, loop=True)
-#    else:
-#        audio.stop()
-#        time.sleep(0.1)
-
-
+chord_C4M = note_C4 + note_E4 + note_G4
 
 KEY_PINS = (
         board.GP1,
         board.GP3,
+        board.GP4,
+        board.GP5,
 )
 
 NOTE_CODES = (
         ('C4', note_C4),
         ('E4', note_E4),
+        ('G4', note_G4),
+        ('chord_C4M', chord_C4M),
 )
 
-#keys = keypad.Keys((board.GP4,), value_when_pressed=False, pull=True)
 keys = keypad.Keys(KEY_PINS, value_when_pressed=False, pull=True)
 
 while True:
