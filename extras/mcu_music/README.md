30f71ed7fdd3f14cbe42cc55fd522f90280f099b
johnedstone <johnedstone@gmail.com>
2023-09-01 20:45:28 -0400

    logic worked out for rhythm or not

--- extras/mcu_music/README.md
+++ extras/mcu_music/README.md
@@ -7,7 +7,19 @@
 * [adafruit switches](https://learn.adafruit.com/make-it-switch/other-types-of-switches)
     * [coding switches](https://learn.adafruit.com/make-it-switch/code-your-micro)
     * [debouncing](https://learn.adafruit.com/key-pad-matrix-scanning-in-circuitpython/keys-one-key-per-pin)
+### Sax-A-Boom wav files
+* Reference: https://github.com/david6983/saxaboom/tree/master
+* ffmpeg (sbLoop.wav is rhythm and is the longest duration):
+    * ffmpeg -i sbLoop.wav -i sbLoop1.wav -filter_complex amix=inputs=2:duration=longest:dropout_transition=0:weights="0.40 1":normalize=0 rhythm_and_track1.mp3 
+    * ffmpeg -i sbLoop.wav -i sbLoop2.wav -filter_complex amix=inputs=2:duration=shortest:dropout_transition=0:weights="0.40 1":normalize=0 rthymn_and_track2_short.mp3
+
+
 
 ### References, not directly relevant
 * [circuitplayground: play tone](https://learn.adafruit.com/circuitpython-made-easy-on-circuit-playground-express/play-tone)
 * [rp2040 PIO CircuitPython](https://learn.adafruit.com/intro-to-rp2040-pio-with-circuitpython/overview)
+
+
+<!---
+# vim: ai et ts=4 sw=4 sts=4 nu
+-->
--- extras/mcu_music/saxaboom_v01.py
+++ extras/mcu_music/saxaboom_v01.py
@@ -9,36 +9,93 @@ import keypad
 
 # range: 0.1 - 1.0
 TONE_VOLUME = 0.5
+CURRENTLY_PLAYING = '' 
 
 audio = AudioOut(board.GP0)
 
-#decoder = audiomp3.MP3Decoder(open("/music/somewhere_2min.mp3", "rb"))
-decoder = audiomp3.MP3Decoder(open("/music/somewhere_01.mp3", "rb"))
+rhythm = audiomp3.MP3Decoder(open("/sax_music/rhythm.mp3", "rb"))
+track1 = audiomp3.MP3Decoder(open("/sax_music/track1.mp3", "rb"))
+track2 = audiomp3.MP3Decoder(open("/sax_music/track2.mp3", "rb"))
+rhythm_and_track1 = audiomp3.MP3Decoder(open("/sax_music/rhythm_and_track1_short.mp3", "rb"))
+rhythm_and_track2 = audiomp3.MP3Decoder(open("/sax_music/rhythm_and_track2_short.mp3", "rb"))
 
+# https://github.com/adafruit/circuitpython/issues/5136
+# Documents clipping
 KEY_PINS = (
-        board.GP6,
+        board.GP1,
+        board.GP2,
+        board.GP3,
+        board.GP4,
+        board.GP5,
 )
 
-NOTE_CODES = (
-        ('Somewhere ...', decoder),
+MP3_CODES = (
+        ('Stop the music', track1),
+        ('Track 1', track1),
+        ('Track 2', track2),
+        ('Rhythm', rhythm),
+        ('Play rhythm and track', ''),
+)
+
+MP3_CODES_RHYTHM = (
+        ('Stop the music', track1),
+        ('Rhythm and Track 1', rhythm_and_track1),
+        ('Rhythm and Track 2', rhythm_and_track2),
+        ('Rhythm', rhythm),
+        ('Play rhythm and track', ''),
 )
 
 keys = keypad.Keys(KEY_PINS, value_when_pressed=False, pull=True)
 
+PLAY_MP3_CODES = True
+CURRENT_KEY_NUMBER = None
+
 while True:
     event = keys.events.get()
-    if event:
-        key, tone = NOTE_CODES[event.key_number]
-        if event.pressed:
-            audio.stop()
-            print(f'playing: {key}')
-            if key == 'Somewhere ...':
-                audio.play(decoder, loop=True)
-            else:
-                pass
+    if event and event.pressed:
 
-        #if event.released:
-        #    print(f'stoping: {key}')
-        #    audio.stop()
+        if event.key_number == 4:
+            PLAY_MP3_CODES = not PLAY_MP3_CODES 
+            # Switch what is being played:
+            print(f'#4: CURRENT_KEY_NUMBER: {CURRENT_KEY_NUMBER}')
+            if CURRENT_KEY_NUMBER not in [0,4]:
+                if PLAY_MP3_CODES:
+                    key, mp3 = MP3_CODES[CURRENT_KEY_NUMBER]
+                    CURRENTLY_PLAYING = key
+                else:
+                    key, mp3 = MP3_CODES_RHYTHM[CURRENT_KEY_NUMBER]
+                    CURRENTLY_PLAYING = key
+
+                audio.stop()
+                audio.play(mp3, loop=True)
+
+                print(f'#4: event.key_number: {event.key_number}')
+                print(f'#4: PLAY_MP3_CODES: {PLAY_MP3_CODES}')
+                print(f'#4: CURRENTLY_PLAYING: {CURRENTLY_PLAYING}')
+            continue
+
+        print(f'event.key_number: {event.key_number}')
+        print(f'PLAY_MP3_CODES: {PLAY_MP3_CODES}')
+
+        if PLAY_MP3_CODES:
+            key, mp3 = MP3_CODES[event.key_number]
+        else:
+            key, mp3 = MP3_CODES_RHYTHM[event.key_number]
+
+        if event.key_number == 0:
+            audio.stop()
+            CURRENTLY_PLAYING = ''
+        elif CURRENTLY_PLAYING != key:
+            audio.stop()
+            CURRENTLY_PLAYING = key
+            audio.play(mp3, loop=True)
+        else:
+            print('Do nothing #1')
+            pass
+
+        CURRENT_KEY_NUMBER = event.key_number
+
+        print(f'END: CURRENTLY_PLAYING: {CURRENTLY_PLAYING}')
+
 
 # vim: ai et ts=4 sts=4 sw=4 nu
